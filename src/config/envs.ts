26d005cc5a623866a9a43e4a36fbd8a37a33b18c
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2025-04-27 16:07:14 -0500

    Added docker

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,7 +5,7 @@ interface EnvVars {
     PORT: number;
     STRIPE_SECRET: string;
     STRIPE_SUCCESS_URL: string;
-    STRIPE_CANCEL_UR: string;
+    STRIPE_CANCEL_URL: string;
     STRIPE_ENDPOINT_SECRET: string;
 }
 
@@ -14,7 +14,7 @@ const envsSchema = joi
         PORT: joi.number().required(),
         STRIPE_SECRET: joi.string().required(),
         STRIPE_SUCCESS_URL: joi.string().required(),
-        STRIPE_CANCEL_UR: joi.string().required(),
+        STRIPE_CANCEL_URL: joi.string().required(),
         STRIPE_ENDPOINT_SECRET: joi.string().required(),
     })
     .unknown(true);
@@ -33,6 +33,6 @@ export const envs = {
     port: envVars.PORT,
     stripeSecret: envVars.STRIPE_SECRET,
     stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
-    stripeCancelUrl: envVars.STRIPE_CANCEL_UR,
+    stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
     stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
 };
